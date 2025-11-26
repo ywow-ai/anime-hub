2baab272240fa605600462d4610169e84b7ca319
ywow-ai <masyaqin37@gmail.com>
2025-11-26 13:42:37 +0700

    init

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./assets/main.css";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed velit
+      molestiae eveniet neque possimus vel placeat consectetur nihil repellat
+      atque doloribus, deleniti earum natus dolores quis pariatur vitae. Itaque,
+      iste?
+    </p>
+  </StrictMode>
+);
