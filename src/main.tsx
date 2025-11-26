266f2e39e81e562fe7e7abf99646d187966fa2ef
ywow-ai <masyaqin37@gmail.com>
2025-11-26 14:15:16 +0700

    feat: Implemented dark mode toggle (Theme: Added theme provider; UI: Integrated mode toggle; Styles: Defined dark and light themes)

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
+import "@/assets/main.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./assets/main.css";
+import { ModeToggle } from "@/components/mode-toggle";
+import { ThemeProvider } from "@/components/theme-provider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed velit
-      molestiae eveniet neque possimus vel placeat consectetur nihil repellat
-      atque doloribus, deleniti earum natus dolores quis pariatur vitae. Itaque,
-      iste?
-    </p>
-  </StrictMode>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed velit
+        molestiae eveniet neque possimus vel placeat consectetur nihil repellat
+        atque doloribus, deleniti earum natus dolores quis pariatur vitae.
+        Itaque, iste?
+      </p>
+      <ModeToggle />
+    </ThemeProvider>
+  </StrictMode>,
 );
